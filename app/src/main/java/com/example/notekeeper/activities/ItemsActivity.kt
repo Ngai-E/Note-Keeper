1a2ff55d1710301440450e455f5a67c34d93e3c7
Ngai Elizabeth <asobingai@gmail.com>
2021-10-11 00:47:18 +0100

    add drawar navigation

---
+++ app/src/main/java/com/example/notekeeper/activities/ItemsActivity.kt
@@ -0,0 +1,68 @@
+package com.example.notekeeper.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.notekeeper.DataManager
+import com.example.notekeeper.R
+import com.example.notekeeper.adapters.NoteRecyclerAdapter
+import com.example.notekeeper.databinding.ActivityItemsBinding
+import com.google.android.material.navigation.NavigationView
+
+class ItemsActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityItemsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityItemsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.appBarItems.toolbar)
+
+        binding.appBarItems.fab.setOnClickListener {
+            startActivity(Intent(this, NoteActivity::class.java))
+        }
+        binding.appBarItems.contentItems.listItems.layoutManager = LinearLayoutManager(this)
+        binding.appBarItems.contentItems.listItems.adapter       = NoteRecyclerAdapter(this, DataManager.notes)
+
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+        val navView: NavigationView = binding.navView
+        val navController = findNavController(R.id.nav_host_fragment_content_items)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+            ), drawerLayout
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.items, menu)
+        return true
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_items)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.appBarItems.contentItems.listItems.adapter?.notifyDataSetChanged()
+    }
+}
