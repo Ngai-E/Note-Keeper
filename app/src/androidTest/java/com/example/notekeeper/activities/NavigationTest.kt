c843cf7033e45c41164cd1ed80b2ee0b42f02c0e
E.Ngai <asobingai@gmail.com>
2021-10-16 23:59:35 +0100

    finish test for drawer navigations

--- app/src/androidTest/java/com/example/notekeeper/activities/NavigationTest.kt
+++ app/src/androidTest/java/com/example/notekeeper/activities/NavigationTest.kt
@@ -1,9 +1,16 @@
 package com.example.notekeeper.activities
 
+import androidx.test.espresso.Espresso.*
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.espresso.contrib.DrawerActions
 import androidx.test.espresso.contrib.NavigationViewActions
 import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.*
+import com.example.notekeeper.CourseInfo
+import com.example.notekeeper.R
+import com.example.notekeeper.adapters.CourseRecyclerAdapter
+import com.example.notekeeper.adapters.NoteRecyclerAdapter
 import org.junit.Assert.*
 import org.junit.Rule
 import org.junit.Test
@@ -18,6 +25,20 @@ class NavigationTest {
 
     @Test
     public  fun selectNoteAfterNavigationDrawerChange() {
+        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open())
+        onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.nav_courses))
 
+        val courseInfoPosition = 0
+        onView(withId(R.id.listItems)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<CourseRecyclerAdapter.ViewHolder>(courseInfoPosition, click())
+        )
+
+        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open())
+        onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.nav_notes))
+
+        val notesPosition = 0
+        onView(withId(R.id.listItems)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<NoteRecyclerAdapter.ViewHolder>(notesPosition, click())
+        )
     }
 }
